62c787c07d6338c2e9f88c1380e60e38c271e58f
Jonas Keinholz <jonas@keinholz.com>
2017-10-25 01:15:24 +0200

    feat: add resolve

--- src/index.js
+++ src/index.js
@@ -1,5 +1,52 @@
-'use strict';
+const datastore = require('@google-cloud/datastore')();
+const storage = require('@google-cloud/storage')();
+const { RequestResolver } = require('@splish-me/serverless-package-registry');
 
-exports.resolve = (request, response) => {
-  response.status(200).send('Hello World!');
+const getVersions = packageName => {
+  console.log('Looking up', packageName);
+  const key = datastore.key(['Package', packageName]);
+
+  return datastore.get(key).then(([pkg]) => pkg.versions);
+};
+
+const resolver = new RequestResolver({ getVersions });
+
+const bucket = storage.bucket('package-registry');
+
+exports.resolve = (req, res) => {
+  const { path } = req;
+
+  if (!path) {
+    console.log('exiting');
+    return res.sendStatus(500);
+  }
+
+  const reqString = path.substr(1);
+
+  console.log('Resolving', reqString);
+
+  resolver
+    .resolve(reqString)
+    .then(fileName => {
+      console.log('Resolved to', fileName);
+
+      if (!fileName) {
+        return res.sendStatus(404);
+      }
+
+      const file = bucket.file(fileName);
+
+      return file.get()
+    })
+    .then(([file]) => {
+      const readStream = file.createReadStream();
+
+      return file.getMetadata().then(([metadata]) => {
+        res.setHeader('content-type', metadata.contentType);
+        readStream.pipe(res);
+      });
+    })
+    .catch(() => {
+      res.sendStatus(404);
+    });
 };
